8299d6f373d02704ffc0c337173955d117b5b8c2
Jonathan Sarry <jsarry@gmail.com>
2024-09-30 17:46:18 +0000

    Add server

--- servers/main.tf
+++ servers/main.tf
@@ -4,5 +4,30 @@ terraform {
       source = "hetznercloud/hcloud"
       version = "~> 1.45"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "hcloud_server" "coolify_server" {
+  name        = "coolify-server"
+  image       = "ubuntu-22.04"
+  server_type = "cx21"  # 2 CPU, 4GB RAM
+  location    = "hel1-dc2"
+  ssh_keys    = var.ssh_keys
+
+  user_data = <<-EOF
+    #cloud-config
+    package_update: true
+    package_upgrade: true
+    runcmd:
+      - curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
+  EOF
+
+  lifecycle {
+    ignore_changes = [ ssh_keys ]
   }
 }
---
+++ servers/variables.tf
@@ -0,0 +1,4 @@
+variable "ssh_keys" {
+    description = "List of public ssh keys to include in server."
+    type = list(string)
+}
